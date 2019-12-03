206ece03046c6f3b7d6c250873c0c0ae8ac465da
Jennifer Cook <kcjenmcook@gmail.com>
2019-12-02 19:13:50 -0600

    initial commit

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Portfolio.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel contactInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }//if - modelstate not valid
+
+            // make the email 
+            string body = string.Format(
+                $"name: {contactInfo.name}<br />"
+                + $"email: {contactInfo.email}<br />"
+                + $"subject: {contactInfo.subject}<br />"
+                + $"message: {contactInfo.message}");
+
+            // create the mailmessage object - System.Net.Mail
+
+            MailMessage msg = new MailMessage(
+                "no-reply@jenmcook.com",
+                "jenmcook@outlook.com",
+                contactInfo.subject, body)
+            {
+                //set MailMessage objects properties
+                IsBodyHtml = true
+            };
+            msg.CC.Add("JenMCook@outlook.com");
+
+            // SMTP client needs created and config'd
+            SmtpClient client = new SmtpClient("mail.jenmcook.com")
+            {
+                Credentials = new NetworkCredential("no-reply@jenmcook.com", "FakePassword"),
+                EnableSsl = false,
+                Port = 8889
+            };
+
+            //use SMTP client object to sent email
+            using (client)
+            {
+                try
+                {
+                    client.Send(msg);
+                    ViewBag.ErrorMessage = "Your message has been sent. Thank you!";
+                    return RedirectToAction("Index", "Home");
+                } // try
+                catch (Exception ex)
+                {
+                    //message if failed to send
+                    ViewBag.ErrorMessage = "An error has occurred in sending your message.\n"
+                        + "Please try again";
+                    return RedirectToAction("Error", "Home");
+                } //catch
+            } // using - cient
+        }
+    }
+}
---
+++ Portfolio/Models/ContactViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class ContactViewModel
+    {
+        //just building a class basically - fields, properties, ctors, method
+        //set up error messages for contact page
+        //first and last name
+        [Required(ErrorMessage = "* Required")]
+        public string name { get; set; }
+
+        //email
+        [Required(ErrorMessage = "* Required")]
+        [EmailAddress(ErrorMessage = "* Please enter a valid email")]
+        [RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$")]
+        public string email { get; set; }
+
+        //subject
+        [StringLength(100, ErrorMessage = "*Less than 100 characters")]
+        public string subject { get; set; }
+
+        //message
+        [Required(ErrorMessage = "* Required")]
+        public string message { get; set; }
+    }
+}
